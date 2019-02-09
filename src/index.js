ee7e24fca9ac3ad87643557ad9489ceb8cda58e8
beeeweee <billdarnalljr@gmail.com>
2019-02-08 21:29:37 -0800

    done - Bill Darnall

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const userObj = {
+    name:'Bill',
+    luckyNumber: luckyNumber()
+    
+}
+
+function luckyNumber(){
+    return Math.floor(Math.random() * 1000);
+}
+
+function greeting(user){
+    console.log(user);
+    return (
+        <div className="container">
+            <h1>{user.name}</h1>
+            <h2 className="text-muted">{user.luckyNumber}</h2>
+        </div>
+    );
+}
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    // <h1 className="container">React Prototypes!</h1>,
+
+        greeting(userObj),
     document.getElementById('root')
 );
